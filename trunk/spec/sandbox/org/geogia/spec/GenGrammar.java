83a609dc571c987d3b9a1ce3be759c5b645ca88e
chanwit <chanwit@8b4fc2e3-1939-0410-a7a8-078e0fc1f4a0>
2007-09-18 04:37:55 +0000

    escape thai unicodes

---
+++ trunk/spec/sandbox/org/geogia/spec/GenGrammar.java
@@ -0,0 +1,31 @@
+package org.geogia.spec;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import org.antlr.Tool;
+
+public class GenGrammar {
+
+	public static void main(String[] args) throws IOException {
+		RandomAccessFile f = new RandomAccessFile("./src/spec.g", "r");
+		byte[] buffer = new byte[(int)f.length()];
+		f.readFully(buffer);
+		char[] charBuffer = new String(buffer, "UTF-8").toCharArray();
+		FileOutputStream fout = new FileOutputStream("./src/spec.g3");
+		for (int i = 0; i < charBuffer.length; i++) {
+			if(charBuffer[i] > 0x0E00) {
+				String s = "\\u0" + Integer.toHexString(charBuffer[i]).toUpperCase();				
+				fout.write(s.getBytes());
+			} else {
+				fout.write(charBuffer[i]);
+			}
+		}
+		fout.close();
+		
+		Tool tool = new Tool();
+		tool.main(new String[]{"-o",".","./src/spec.g3"});		
+	}
+	
+}
