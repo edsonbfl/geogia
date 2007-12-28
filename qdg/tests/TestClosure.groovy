93ad3ca351da138087cd7829effa5a5fa6be2d7a
chanwit <chanwit@8b4fc2e3-1939-0410-a7a8-078e0fc1f4a0>
2007-12-28 02:03:27 +0000

    updated test case

--- qdg/tests/TestClosure.groovy
+++ qdg/tests/TestClosure.groovy
@@ -1,8 +1,14 @@
+import static Math.*
+import static org.codehaus.qdg.runtime.DefaultMethods.*
 
 class TestClosure {
 
 	def a = {
-		println 'test'
+//		println 'test 123'
+		println sin(123.456) * cos(456.789)		
+		100.times {
+			println it
+		}
 	}
 
 }
