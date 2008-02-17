aaedcd1d497d650f9aa34ae246ef64155ac97c42
chanwit <chanwit@8b4fc2e3-1939-0410-a7a8-078e0fc1f4a0>
2008-02-17 18:03:59 +0000

    imported tspec

---
+++ trunk/spec/new_src/org/tspec/hangman/Hangman.java
@@ -0,0 +1,43 @@
+package org.tspec.hangman;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hangman {
+	
+	private String word;
+	private int wrongs;
+	private int maxGuess;
+	private List<String> unrevealedWord;
+	private boolean finished;
+	
+	public boolean isFinished() {
+		return finished;
+	}
+
+	public List<String> getUnrevealedWord() {
+		return unrevealedWord;
+	}
+
+	public int getMaxGuess() {
+		return maxGuess;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+		unrevealedWord = new ArrayList<String>();
+		for(int i=0;i<word.length();i++) {
+			unrevealedWord.add("_");
+		}
+		this.maxGuess = 12;
+	}
+
+	public int getWrongs() {
+		return wrongs;
+	}
+
+}
