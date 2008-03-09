5d919cb62fba984947d46c208f7cacaef6be416c
chanwit <chanwit@8b4fc2e3-1939-0410-a7a8-078e0fc1f4a0>
2008-03-09 15:41:19 +0000

    refactored should and must objects

--- spec/src/java/org/tspec/runtime/MustObject.java
+++ spec/src/java/org/tspec/runtime/MustObject.java
@@ -3,11 +3,11 @@ package org.tspec.runtime;
 public class MustObject {
 
 	private Object object;
-	private boolean not=false;
+	private boolean not;
 
 	public MustObject(Object o) {
-		this.object = o;
-	}
+		this(o, false);
+	}	
 	
 	public MustObject(Object o, boolean b) {
 		this.object = o;
@@ -16,41 +16,18 @@ public class MustObject {
 
 	@Override
 	public boolean equals(Object obj) {
+		boolean result = this.object.equals(obj);
 		if(this.not==true) {
-			if(this.object.equals(obj)) {
-				for(StackTraceElement ste: Thread.currentThread().getStackTrace()) {
-					if(ste.getFileName()!=null && ste.getFileName().lastIndexOf("Story.groovy")!=-1) {
-						ErrorListener.v().addError(
-							ste.getFileName()+":"+ste.getLineNumber() + " / "
-							+ this.object + " must not be " + obj
-						);
-						break;
-					}				
-				}
-				return false;
-			} else {
-				return true;
-			}						
+			if(!result) ReportHelper.reportError(this.object, obj, "must not be");
+			return !result;
 		} else {
-			if(!this.object.equals(obj)) {
-				for(StackTraceElement ste: Thread.currentThread().getStackTrace()) {
-					if(ste.getFileName()!=null && ste.getFileName().lastIndexOf("Story.groovy")!=-1) {
-						ErrorListener.v().addError(
-							ste.getFileName()+":"+ste.getLineNumber() + " / "
-							+ this.object + " must be " + obj
-						);
-						break;
-					}				
-				}
-				return false;
-			} else {
-				return true;
-			}
+			if(result) ReportHelper.reportError(this.object, obj, "must be");
+			return result;
 		}
 	}
 	
 	public MustObject getNot() {
 		return new MustObject(this.object, true);
-	}
+	}		
 
 }
