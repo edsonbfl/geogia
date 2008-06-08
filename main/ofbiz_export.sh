f051ce24e58586e470eb07c2ed9d737641c1a30e
chanwit <chanwit@8b4fc2e3-1939-0410-a7a8-078e0fc1f4a0>
2008-06-08 15:27:23 +0000

    updated exporting script

--- main/ofbiz_export.sh
+++ main/ofbiz_export.sh
@@ -1,1 +1,4 @@
-svn export http://svn.apache.org/repos/asf/ofbiz/trunk/applications/ ofbiz/.
+svn export --force http://svn.apache.org/repos/asf/ofbiz/trunk/applications/ ofbiz/applications/.
+mkdir ofbiz/framework
+mkdir ofbiz/framework/entity
+svn export --force http://svn.apache.org/repos/asf/ofbiz/trunk/framework/entity/ ofbiz/framework/entity/.
